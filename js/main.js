4b56d1180bee2caa34401da03fff7bf0ffc23ff4
Faisal Ahmed <81064609+Faisal-Ahmed100@users.noreply.github.com>
2021-03-21 11:01:18 +0600

    New chance

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+var preloderAll= document.getElementById("preloading");
+function
+	myFunction(){
+		preloderAll.style.display="none";
+		
+	};
+
+
+ $(document).ready(function(){
+    $('.sliderWapper').bxSlider();
+  });
+
+$(document).ready(function(){
+	$(".menu_bar_aria").meanmenu({
+			meanMenuContainer:"#mobiledropdwanmenu",
+			meanScreenWidth:991,
+			
+			
+		})
+	
+});
+
+
+
+
+
